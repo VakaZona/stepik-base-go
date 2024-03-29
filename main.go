0c52a58e36916c108325ba5fef39ced9e01cf1ba
Valery Zonov <valery.zonov@sibers.com>
2024-03-29 18:24:21 +0700

    1.5 Переменные, операции

--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("I like Go!")
-	fmt.Println("I like Go!")
-	fmt.Println("I like Go!")
+	var number int
+	fmt.Scan(&number)
+	number = (number * 2) + 100
+	fmt.Println(number)
 }
