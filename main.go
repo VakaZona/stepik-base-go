1d7ad2cd7234aeb5833008363f478d50d449ec96
Valery Zonov <valery.zonov@sibers.com>
2024-04-01 12:06:44 +0700

    1.9 Условные конструкции

--- main.go
+++ main.go
@@ -1,14 +1,15 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	var d int
-	fmt.Scan(&d)
-	hour := d / 30
-	minute := (d % 30) * 2
-
-	fmt.Println("It is", hour, "hours", minute, "minutes.")
+	var a int
+	fmt.Scan(&a)
+	if a > 0 {
+		fmt.Println("Число положительное")
+	} else if a < 0 {
+		fmt.Println("Число отрицательное")
+	} else {
+		fmt.Println("Ноль")
+	}
 }
