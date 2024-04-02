dc03ae29265b25e73b160180fcb96a36e86ebf5c
Valery Zonov <valery.zonov@sibers.com>
2024-04-02 21:35:03 +0700

    1.10 Циклы

--- main.go
+++ main.go
@@ -3,17 +3,15 @@ package main
 import "fmt"
 
 func main() {
+	var n int
 	var number int
-	fmt.Scan(&number)
-	if 0 != number/10000 {
-		fmt.Print(number / 10000)
-	} else if 0 != number/1000 {
-		fmt.Println(number / 1000)
-	} else if 0 != number/100 {
-		fmt.Println(number / 100)
-	} else if 0 != number/10 {
-		fmt.Println(number / 10)
-	} else {
-		fmt.Println(number)
+	var sum int = 0
+	fmt.Scan(&n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&number)
+		if (number%8 == 0) && (number >= 10) && (number <= 99) {
+			sum += number
+		}
 	}
+	fmt.Println(sum)
 }
